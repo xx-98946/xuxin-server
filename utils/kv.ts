ae8879081be546173c485da15071126f3fe7df00
xuxin <2821550908@qq.com>
2024-04-05 22:27:46 +0800

    fix: 修改env

--- utils/kv.ts
+++ utils/kv.ts
@@ -1,4 +1,20 @@
-const kv = await Deno.openKv("https://api.deno.com/databases/b9d5cd3c-af18-4bab-86b4-22dbf268db7b/connect");
 
+import { load } from "std/dotenv/mod.ts";
 
+/**
+ * 区分开发环境和部署环境
+ * 部署环境不允许设置
+ * @returns
+ */
+async function getKV() {
+    const env = await load();
+    if (env.MODE) {
+        return Deno.openKv(
+            "https://api.deno.com/databases/b9d5cd3c-af18-4bab-86b4-22dbf268db7b/connect",
+        );
+    }
+    return Deno.openKv();
+}
+
+const kv = await getKV();
 export default kv;
